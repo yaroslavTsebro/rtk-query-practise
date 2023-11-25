b98acd886cdc260c261c23fedbbbedecb2f3b899
nafef60552 <nafef60552@qianhost.com>
2023-11-25 18:50:42 +0200

    added routing

--- src/features/routing.tsx
+++ src/features/routing.tsx
@@ -1,15 +1,43 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import PostsList from "./Post/PostsList/PostsList";
+import UsersList from "./User/UserList/UsersList";
+import UserPage from "./User/UserPage/UserPage";
 import RootLayout from "./layout/RootLayout/RootLayout";
+import AddPostForm from "./Post/AddPostForm/AddPostForm";
+import SinglePostPage from "./Post/SinglePostPage/SinglePostPage";
+import EditPostForm from "./Post/EditPostForm/EditPostForm";
+
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <RootLayout/>,
+    element: <RootLayout />,
     children: [
       {
         index: true,
-        element: 
-      }
-    ]
+        element: <PostsList />,
+      },
+      {
+        path: "posts",
+        children: [
+          { index: true, element: <AddPostForm /> },
+          { path: ":postId", element: <SinglePostPage /> },
+          { path: "edit/:postId", element: <EditPostForm /> }
+        ],
+      },
+      {
+        path: "users",
+        children: [
+          { index: true, element: <UsersList /> },
+          { path: ":userId", element: <UserPage /> }
+        ],
+      },
+    ],
+  },
+  // Catch all route for 404 or redirection
+  {
+    path: "*",
+    element: <Navigate to="/" replace />
   }
-])
+]);
+
